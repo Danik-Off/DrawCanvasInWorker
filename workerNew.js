71ea1ff5fa9f17ce85a626597c6374da2ecb5791
Danik-Off <52536804+Danik-Off@users.noreply.github.com>
2023-02-22 10:55:04 +0300

    добавлено через onmessage
    отрисовка в основном потоке  через события worker, в worker высчитываются координаты

--- workerNew.js
+++ workerNew.js
@@ -1,11 +1,17 @@
 onmessage = (e) => {
     let x = e.data.x;
     let y = e.data.y;
-
-    setInterval(draw, 10);
-
-    function draw() {
-        onmessage({ x: x, y: y });
-        
+    let speed = e.data.speed;
+    const width= e.data.width;
+    setInterval(draw,10)
+    function draw()
+    {
+        if(x>=width||x<=0)
+        {
+            speed =-speed;
+        }
+        x+=speed;
+        postMessage({x:x,y:y,speed:speed});
     }
+  
 }
